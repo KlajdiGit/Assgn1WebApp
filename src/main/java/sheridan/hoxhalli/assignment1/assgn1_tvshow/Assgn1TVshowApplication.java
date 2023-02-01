cfb2042b2c271ceafda2ed4b860536db0357bc26
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-01-31 19:08:41 -0500

    Created the basic folders and html templates to start out work

---
+++ src/main/java/sheridan/hoxhalli/assignment1/assgn1_tvshow/Assgn1TVshowApplication.java
@@ -0,0 +1,13 @@
+package sheridan.hoxhalli.assignment1.assgn1_tvshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assgn1TVshowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assgn1TVshowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/hoxhalli/assignment1/assgn1_tvshow/Assgn1TVshowApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.hoxhalli.assignment1.assgn1_tvshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assgn1TVshowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
